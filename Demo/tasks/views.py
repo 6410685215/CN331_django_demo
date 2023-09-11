3339e1bb0c37833682db938794368bf18cf3caa6
partyTeaTime <6410685215@student.tu.ac.th>
2023-09-12 01:12:20 +0700

    WEB_demo

---
+++ Demo/tasks/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+tasks = ["task1", "task2", "task3"]
+# Create your views here.
+
+def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    
+    return render(request, "tasks/index.html", {'tasks': request.session["tasks"]})
+
+class newTaskForm(forms.Form):
+    task = forms.CharField(label="New Task", min_length = 8)
+    
+def add(request):
+    if request.method == "POST":
+        form = newTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {"form": form})
+    return render(request, "tasks/add.html", {"form": newTaskForm()})
